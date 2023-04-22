96263761c3b23ba95afc4010836462b850e997ca
prog5 <prog5@BASICHOME>
2023-04-23 01:35:24 +0300

    Добавьте файлы проекта.

---
+++ ResponseJob/Figure.cs
@@ -0,0 +1,47 @@
+﻿namespace ResponseJob
+{
+    /// <summary>
+    /// General Figure Class
+    /// </summary>
+    public class Figure
+    {
+        public static class Circle 
+        {   
+            public static double CalculationArea(int r) => Math.Round((Math.PI * r * r), 3);
+        }
+
+        public class Triangle
+        {
+            public static double CalculationArea(int[] data)
+            {
+                if (data[2] >= data[0] + data[1] || data[1] >= data[0] + data[2] || data[0] >= data[1] + data[2] )
+                {
+                    throw new Exception("Треугольника несуществует");
+                }
+
+                double p = (data[0] + data[1] + data[2]) / 2.0;
+
+                // Проверка согласно условию задания, но кажется куда логичным вытянуть это все с отдельный метод, собственно его пример ниже
+                if (data[0] == data[1] || data[0] == data[2] || data[1] == data[2])
+                {
+                    Console.WriteLine("Равнобедренный");
+                }
+
+                return Math.Round(Math.Sqrt(p * (p - data[0]) * (p - data[1]) * (p - data[2])), 3);
+            }
+
+            private void Check(int[] data)
+            {
+                if (data[0] == data[1] || data[0] == data[2] || data[1] == data[2])
+                {
+                    Console.WriteLine("Равнобедренный");
+                }
+                else if(data[0] == data[1] && data[2] == data[1])
+                {
+                    Console.WriteLine("Равносторонний");
+                }
+                // etc
+            }
+        }
+    }
+}
---
+++ TestFigure/UnitTest1.cs
@@ -0,0 +1,82 @@
+using static ResponseJob.Figure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestFigure
+{
+    [TestClass]
+    public class TestFigure
+    {
+        [TestMethod]
+        public void Test_AreaCalc_Circle()
+        {
+            List<double> expected = new();
+            List<int> rad = new();
+            List<double> res = new();
+
+            // Добавление уже готового результата
+            for (int i = 0; i < File.ReadAllLines(@"info\CircleExpArea.txt").Length; i++)
+            {
+                var temp = (File.ReadAllLines(@"info\CircleExpArea.txt"));
+
+                expected.Add(double.Parse(temp[i]));
+            }
+
+            // Добавление радиуса
+            for (int i = 0; i < File.ReadAllLines(@"info\RCircle.txt").Length; i++)
+            {
+                var temp = (File.ReadAllLines(@"info\RCircle.txt"));
+
+                rad.Add(int.Parse(temp[i]));
+            }
+
+            // Занесение результата из библиотеки в лист
+            foreach (var e in rad)
+            {
+                res.Add(Circle.CalculationArea(e));
+            }
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], res[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Test_AreaCalc_Triangle()
+        {
+            List<double> expected = new();
+            List<int[]> sides = new();
+            List<double> res = new();
+
+            // Добавление уже готового результата
+            for (int i = 0; i < File.ReadAllLines(@"info\TrangleExpArea.txt").Length; i++)
+            {
+                var temp = (File.ReadAllLines(@"info\TrangleExpArea.txt"));
+
+                expected.Add(double.Parse(temp[i]));
+            }
+
+            // Добавление радиуса
+            for (int i = 0; i < File.ReadAllLines(@"info\TriangleSides.txt").Length; i++)
+            {
+                var temp = (File.ReadAllLines(@"info\TriangleSides.txt"));
+                var t = (temp[i].Split(@"\"));
+                int[] side = { int.Parse(t[0]), int.Parse(t[1]), int.Parse(t[2]) };
+
+
+                sides.Add(side);
+            }
+
+            // Занесение результата из библиотеки в лист
+            foreach (var e in sides)
+            {
+                res.Add(Triangle.CalculationArea(e));
+            }
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], res[i]);
+            }
+        }
+    }
+}
